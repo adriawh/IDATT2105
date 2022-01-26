e64aaba0a1737e8cd8097594c09d5020e953934d
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-01-26 17:07:13 +0100

    deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/adriawh/idatt2105-fullstack_appliksjonsutvikling/.git master:gh-pages
+git push -f https://github.com/adriawh/idatt2105-fullstack_appliksjonsutvikling.git master:gh-pages
 cd -
