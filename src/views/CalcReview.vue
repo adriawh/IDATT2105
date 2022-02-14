c5cc678dffa12e6d667a8f1a5bd806a18e352f8d
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-02-14 14:26:02 +0100

    Started oppgave 1

--- src/views/CalcReview.vue
+++ src/views/CalcReview.vue
@@ -2,19 +2,37 @@
   <div class="container">
     <h1>Calculator feedback</h1>
 
-    <BaseInput label="Navn" type="text" />
-    <BaseInput label="Epost" type="text" />
+    <form @submit.prevent="sendForm">
+      <BaseInput label="Navn" type="text" v-model="user.name" />
+      <BaseInput label="Epost" type="email" v-model="user.email" />
+      <BaseInput label="Melding" type="text" v-model="user.message" />
+
+      <button type="submit">Submit</button>
+    </form>
+    <pre>
+    {{ user }}
+  </pre
+    >
   </div>
 </template>
 
 <script>
-import BaseInput from "../components/BaseInput.vue";
 
+import { useField } from 'vee-validate';
+import axios from "axios";
 export default {
-  components: {
-    BaseInput,
-  },
 
+  setup(){
+    function onSubmit(){
+      alert('Submitted')
+    }
+
+    const email = useField('email', function(value){
+      if(!value) return 'This field is required'
+
+      return true
+    })
+  },
   data() {
     return {
       user: {
@@ -23,6 +41,18 @@ export default {
         message: "",
       },
     };
+  },
+  methods: {
+    sendForm() {
+      axios
+        .post()
+        .then(function (response) {
+          console.log("Response", response);
+        })
+        .catch(function (err) {
+          console.log("Error", err);
+        });
+    },
   },
 };
 </script>
